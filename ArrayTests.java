965213a7ca050697496f3a07dc57069ef393b293
Yshzi <83853443+Yshzi@users.noreply.github.com>
2023-01-30 01:22:24 -0800

    Added two tests.

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace1() {
+    int[] input = {3, 1, 2};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {2, 1, 3}, input);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReversed1() {
+    int[] input = {1 ,2 , 3};
+    assertArrayEquals(new int[] {3, 2, 1}, ArrayExamples.reversed(input));
+  }
+  
 }
